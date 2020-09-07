47d619140d183f13a97316e3be4d1b711a785551
fangcong <765494646@qq.com>
2020-09-08 01:39:09 +0800

    feat:cidr2netmask

--- net/network.go
+++ net/network.go
@@ -9,3 +9,38 @@
 // See the Mulan PSL v2 for more details.
 
 package net
+
+import (
+	"errors"
+
+	"github.com/gakkiyomi/galang/utils"
+)
+
+func (*GalangNet) NetmaskToCIDR(mask string) (int, error) {
+	//TODO
+	return 0, nil
+}
+
+func (*GalangNet) CIDRToNetmask(cidr int) (string, error) {
+
+	if cidr < 0 || cidr > 32 {
+		return "", errors.New("cidr must be less than 32 and more than 0")
+	}
+
+	mask := (0xFFFFFFFF << (32 - cidr)) & 0xFFFFFFFF
+	var dmask uint64
+	dmask = 32
+	var localmask string
+	for i := 1; i <= 4; i++ {
+		tmp := mask >> (dmask - 8) & 0xFF
+		tmpStr := utils.Transform.IntToString(tmp)
+		if i == 1 {
+			localmask = tmpStr
+		} else {
+			localmask = localmask + "." + tmpStr
+		}
+
+		dmask -= 8
+	}
+	return localmask, nil
+}
