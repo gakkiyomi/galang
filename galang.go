53c138f197b409ff0c76eb4bc8ef775912aeadd2
fangcong <fangcong@sky-cloud.net>
2020-09-25 11:15:58 +0800

    'fix'

--- galang.go
+++ galang.go
@@ -12,86 +12,16 @@ package main
 
 import (
 	"flag"
-	"regexp"
-	"sync"
-	"time"
 
-	"github.com/alouca/gosnmp"
 	"github.com/gakkiyomi/galang/net"
-	"github.com/songtianyi/rrframework/logs"
 )
 
 func main() {
 
 	var target string
-	flag.StringVar(&target, "t", "", "目标地址集合")
+	flag.StringVar(&target, "n", "192.168.1.0/24", "目标地址集合")
 	flag.Parse()
 
-	is_Range, _ := regexp.MatchString(net.RANGE_REG, target)
-	is_Ip, _ := regexp.MatchString(net.IP_REG, target)
-	is_Cidr, _ := regexp.MatchString(net.CIDR_REG, target)
+	net.SNMP.Sacn(target)
 
-	if is_Range == true {
-		list, err := net.Network.GetRangeAddrList(target)
-		if err != nil {
-			logs.Error(err.Error())
-		}
-		for _, t := range list {
-			start(t)
-		}
-
-		return
-	}
-
-	if is_Ip == true {
-		start(target)
-		return
-	}
-
-	if is_Cidr == true {
-		var wg sync.WaitGroup
-		list, err := net.Network.GetCIDRAvailableAddrList(target)
-		if err != nil {
-			logs.Error(err.Error())
-		}
-		wg.Add(len(list))
-		for _, t := range list {
-			go startSync(t, &wg)
-		}
-		wg.Wait()
-		return
-	}
-
-}
-
-func start(target string) {
-
-	wrapper, err := net.SNMP.NewSnmpWrapper(target, "public", gosnmp.Version2c, 3)
-	if err != nil {
-		logs.Error(err.Error())
-	}
-	intf, err := wrapper.Interfaces()
-	if err == nil {
-		for _, v := range intf {
-			time.Sleep(time.Duration(500) * time.Millisecond)
-			logs.Info(v.ToString())
-		}
-	}
-}
-
-func startSync(target string, wg *sync.WaitGroup) {
-
-	defer wg.Done()
-
-	wrapper, err := net.SNMP.NewSnmpWrapper(target, "public", gosnmp.Version2c, 3)
-	if err != nil {
-		logs.Error(err.Error())
-	}
-	intf, err := wrapper.Interfaces()
-	if err == nil {
-		for _, v := range intf {
-			time.Sleep(time.Duration(500) * time.Millisecond)
-			logs.Info(v.ToString())
-		}
-	}
 }
