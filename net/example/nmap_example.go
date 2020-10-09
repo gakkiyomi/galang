4304adbb4cc5a2b5ea8b9a30c69eda7cc5533bcb
fangcong <fangcong@sky-cloud.net>
2020-10-09 12:00:57 +0800

    'example'

--- net/example/nmap_example.go
+++ net/example/nmap_example.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/Ullaakut/nmap"
@@ -11,7 +12,7 @@ import (
 )
 
 func main() {
-	getHosts(`192.168.1.146`)
+	getInterfaces(`192.168.1.146`)
 }
 
 func getHosts(target string) {
@@ -44,4 +45,60 @@ func getHosts(target string) {
 
 	b2, _ := json.Marshal(res.Hosts())
 	logs.Info(string(b2))
+}
+
+func baseScan(target string) {
+	sc, err := net.NMAP.NewScanner(target, "fangcong")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	if err != nil {
+		logs.Error("unable to create nmap scanner: %v", err)
+	}
+
+	b, _ := json.Marshal(sc)
+
+	logs.Info("===========================================================")
+	logs.Info(string(b))
+	logs.Info("===========================================================")
+
+	do := make(chan *net.Scanner, 1)
+
+	go func() {
+		logs.Info("===========================================================")
+		logs.Info("Runnning")
+		logs.Info("===========================================================")
+		sc.Status = net.Running
+		sc, err = sc.Scanner(
+			nmap.WithPorts("80", "8080", "5432"),
+			nmap.WithContext(ctx),
+		)
+		if err != nil {
+			logs.Error("scan error : %v", err)
+		}
+		do <- sc
+	}()
+
+	res := <-do
+
+	result := res.Result
+	// Use the results to print an example output
+	for _, host := range result.Hosts {
+		if len(host.Ports) == 0 || len(host.Addresses) == 0 {
+			continue
+		}
+		fmt.Printf("addresses %q ", host.Addresses)
+		fmt.Printf("Host %q , type %q ,verdor %q:\n", host.Addresses[0].Addr, host.Addresses[0].AddrType, host.Addresses[0].Vendor)
+		for _, port := range host.Ports {
+			fmt.Printf("\tPort %d/%s %s %s\n", port.ID, port.Protocol, port.State, port.Service.Name)
+		}
+	}
+
+	fmt.Printf("Nmap done: %d hosts up scanned in %3f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
+
+	b2, _ := json.Marshal(sc)
+	logs.Info("===========================================================")
+	logs.Info(string(b2))
+	logs.Info("===========================================================")
 }
