985582ac4a3206fe74f8338c3c9c90c73c670c61
fangcong <fangcong@sky-cloud.net>
2020-10-09 17:47:08 +0800

    feat sort

--- galang.go
+++ galang.go
@@ -8,4 +8,14 @@
 // THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
-package galang
+package main
+
+import (
+	"github.com/gakkiyomi/galang/sort"
+)
+
+func main() {
+	source := []int{1, 4, 2, 44, 22, 13, 222, 441, 3, 54}
+	sort.BubbleSort(source)
+
+}
